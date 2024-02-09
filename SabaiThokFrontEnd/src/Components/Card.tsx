bd01212ad3f4dbcb50ac0e4306a1d92574c6c283
Suyogya17 <suyogyashrestha17@gmail.com>
2024-02-09 08:30:05 +0545

    bttn

--- SabaiThokFrontEnd/src/Components/Card.tsx
+++ SabaiThokFrontEnd/src/Components/Card.tsx
@@ -1,24 +1,35 @@
-import "./Card.css"
+import "./Card.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface Product {
-    id: number;
-    productImage: string;
-    productName: string;
-    price: number;
-  }
+  id: number;
+  productImage: string;
+  productName: string;
+  price: number;
+}
 
-  
-
-function Card (){
-    return(
-        <>
-         <div className="cardbox">
-            
-            </div>
-        </>
-    );
+function Card() {
+  return (
+    <>
+      <div className="cardbox">
+        <div className="productimg">
+          <img
+            src={`https://www.pexels.com/photo/black-fujifilm-dslr-camera-90946/`}
+            width={100}
+            height={100}
+            alt="productImg"
+          />
+        </div>
+        <div className="productdiscription">
+            <p>This is the image of the product and here is the discription</p>
+        </div>
+        <div className="bttntocart">
+            <button>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
