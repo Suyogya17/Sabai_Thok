9e5ee0c0c46b68b1e40203951c9f0e2d5a0be3a1
Suyogya17 <suyogyashrestha17@gmail.com>
2024-02-20 11:08:41 +0545

    apple

---
+++ src/main/java/com/example/sabai_thok/Entity/Category.java
@@ -0,0 +1,22 @@
+package com.example.sabai_thok.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="category")
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String categoryName;
+}
+
